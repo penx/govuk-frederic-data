c5dfd62f5b593a1cdd1e405ce59fcd747997f484
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-10 11:52:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const filters = require('./filters')
+const result = require('./result');
+const results = require('./results');
+const search = require('./search');
+
+module.exports = {
+  filters,
+  result,
+  results,
+  search
+}
